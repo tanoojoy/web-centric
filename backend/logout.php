cc1730187be4f0d7ced1d428d8760101583982d9
yashnamotee <moteeyashna@gmail.com>
2024-10-02 21:55:45 +0400

    backend

---
+++ backend/logout.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+
+if (isset($_POST['logout'])) {
+    
+    session_unset(); // Unset all session variables
+    session_destroy(); // Destroy the session itself
+
+    header("Location: ../");
+    exit();
+}
+?>
