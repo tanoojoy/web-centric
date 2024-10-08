bad2f34ff03a0d9f74d698e1314f26607fb92d83
yashnamotee <moteeyashna@gmail.com>
2024-10-08 16:22:01 +0400

    Revert "updated navbar"
    This reverts commit 2c9db9c20527b93ffaa413c11522f46958ec3a5f.

--- components/navbar.php
+++ components/navbar.php
@@ -3,9 +3,7 @@
         <a href="#" class="logo">Talent<b> Hub</b></a>
         <ul class="links">
             <li><a href="/">Home</a></li>
-            <li>
-                <a href="aboutUs.php">About Us</a>
-            </li>
+            <li>About Us</li>
             <li>Work</li>
             <li>
                 <a href="login.php">Login</a>
