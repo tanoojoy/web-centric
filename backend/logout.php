6080228f2105806c90d20fc11c8528c93df6d7e0
Tanoo Joyekurun <tanoo_joy@hotmail.com>
2024-10-10 23:57:05 +0400

    fix sa ggt logout la

--- backend/logout.php
+++ backend/logout.php
@@ -1,9 +1,10 @@
 <?php
+    if (isset($_POST['logout'])) {
+        session_unset(); // Unset all session variables
+        session_destroy(); // Destroy the session itself
 
-    session_unset(); // Unset all session variables
-    session_destroy(); // Destroy the session itself
-
-    header("Location: ../index.php");
-    exit();
+        header("Location: ../index.php");
+        exit();
+    }
 
 ?>
