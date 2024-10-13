d0e7d0c61de8ac472dfea0c6fdbe9ae32e750d2b
yashnamotee <moteeyashna@gmail.com>
2024-10-13 16:16:11 +0400

    education

--- profile.php
+++ profile.php
@@ -50,6 +50,7 @@
     </nav>
 </header>
 
+
 <div class="profile-container">
     <form action="backend/update_profile.php" method="POST" enctype="multipart/form-data">
         <div class="profile-pic-section">
@@ -99,9 +100,50 @@
 
             <label for="current_company"><strong>Current Company:</strong></label>
             <input type="text" name="current_company" id="current_company">
+        
+            
+            <div class="education">
+            <h3>Education Details</h3>
 
-            <label for="education"><strong>Education:</strong></label>
-            <input type="text" name="education" id="education">
+            <label for="major"><strong>Degree:</strong></label>
+            <input type="text" name="major" id="major">
+
+            <label for="level"><strong>Level:</strong></label>
+            <select name="level" id="level">
+                <option value="Sc">Sc</option>
+                <option value="Hsc">Hsc</option>
+                <option value="Diploma">Diploma</option>
+                <option value="Bachelors">Bachelors</option>
+                <option value="Masters">Masters</option>
+                <option value="Phd">Phd</option>
+            </select>
+
+            <label for="institution"><strong>Institution:</strong></label>
+            <input type="text" name="institution" id="institution" >
+
+            <label for="field of study"><strong>Field of study:</strong></label>
+            <input type="text" name="field of study" id="field of study">
+
+            <label for="start date"><strong>Start date:</strong></label>
+            <input type="date" id="start date" name="school-start" value="2024-10-13" min="2000-01-01" max="2025-01-01" />
+
+
+            <label for="end date"><strong>End date:</strong></label>
+            <input type="date" id="end date" name="school-end" value="2024-10-13" min="2000-01-01" max="2025-01-01" />
+
+            <label for="gpa"><strong>GPA:</strong></label>
+            <input type="text" name="gpa" id="gpa">
+
+            <label for="activities"><strong>Activities:</strong></label>
+            <input type="text" name="activities" id="activities">
+
+            <label for="description"><strong>Decription:</strong></label>
+            <input type="text" name="description" id="description">
+
+
+        </div>
+    
+
 
             <label for="skills"><strong>Skills:</strong></label>
             <select name="skills" id="skills" multiple>
