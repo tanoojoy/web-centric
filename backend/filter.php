2729b5aa470b68e86e06aea9184834e1657e431e
Tanoo Joyekurun <tanoo_joy@hotmail.com>
2025-04-22 00:20:14 +0400

    finished JSON schema bullshit

--- backend/filter.php
+++ backend/filter.php
@@ -1,36 +1,32 @@
 <?php
-require 'vendor/autoload.php';
-use Opis\JsonSchema\Validator;
 include "functions.php";
+require '../vendor/autoload.php';
+
+use Opis\JsonSchema\{
+    Validator,
+    ValidationResult,
+    Errors\ErrorFormatter,
+};
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $json = file_get_contents('php://input');
 
-    $validation_data = json_decode($json);
-    $data = json_decode($json, true);
+    $validation_data = json_decode($json);  //get the post data for validationn
+    $data = json_decode($json, true);       //get the post data as associative array for filter_jobs()
 
-    // $json_schema_json = file_get_contents("filter_schema.json");
-    // $json_schema = json_decode($json_schema_json);
+    $json_schema = json_decode(file_get_contents("filter_schema.json"));    //fetch and decode the schema from file
 
-    // if (json_last_error() !== JSON_ERROR_NONE) {
-    //     echo "Schema JSON decoding error: " . json_last_error_msg();
-    //     exit;
-    // }
+    $validator = new Validator();
+    $result = $validator->validate($validation_data, $json_schema); //validate stuff
 
-    // // Log the raw JSON schema string instead of object
-    // file_put_contents("backend/error_log.txt", $json_schema_json);
-
-    // $validator = new Validator();
-    // $result = $validator->validate($validation_data, $json_schema);
-
-    // if ($result->isValid()) {
-    //     echo filter_jobs($data);
-    // } else {
-    //     $formatter = new ErrorFormatter();
-    //     $errors = $formatter->format($result->getErrors());
-    //     echo "Invalid JSON filter received.\n";
-    //     print_r($errors);
-    // }
+    if ($result->isValid()) {
+        // echo "hi";
+        echo filter_jobs($data);
+    } else {
+        foreach ($result->getErrors() as $error) {
+            echo "Error at: " . $error->dataPointer() . "\n";
+            echo "Message: " . $error->keyword() . " - " . $error->keywordArgs()['message'] ?? 'Validation error' . "\n\n";
+        }
+    }
 }
 ?>
-
